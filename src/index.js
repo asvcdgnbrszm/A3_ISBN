30e68b9dde47d564ccdc93169b6d1014f9e7eb7a
asvcdgnbrszm <n1ckel@web.de>
2021-12-12 17:03:30 +0100

    fullfills task b

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ const trigger = document.getElementById("btnStart");
 
 trigger.addEventListener('click', () => {
 
-    const isbnString = document.querySelector("#inputIsbn").value;
+    //const isbnString = document.querySelector("#inputIsbn").value;
     const outDiv = document.querySelector('#outDiv');
 
-    result = checkISBN(isbnString,2);
+    result = checkISBN('1111111112',2);
 
     if (result) { 
         outDiv.innerHTML = 'Die ISBN-Nummer ist korrekt.'
